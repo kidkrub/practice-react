99aa295edcc5278da0a6f0a02f18bbe2b8efc693
unknown <kritsadamoungkhao@gmail.com>
2020-03-12 00:32:50 +0700

    end of day

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import InputBox from './components/InputBox';
+import Focus from './components/Focus';
+import GetFile from './components/GetFile';
+import ChildrenComp from './components/ChildrenComp';
+
+const childrencomponent = React.createRef<ChildrenComp>()
+const handleClick = () => {
+  childrencomponent.current?.showAlert()
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <InputBox name="name" />
+      <InputBox name="email" />
+      <Button name="App"/>
+      <Focus />
+      <GetFile />
+      <ChildrenComp ref={childrencomponent}/>
+      <button onClick={handleClick}>Get Component</button>
     </div>
   );
 }
